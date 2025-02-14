5078b72f1c8843faf691d8fe2c859e084a84b69b
Michael Rayven <themikerayven@gmail.com>
2025-02-14 11:27:27 +0700

    Lab3

--- lab2_g.c
+++ lab2_g.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
 int readInt(int *number) {
     char line[64];
@@ -103,43 +105,55 @@ void printData(int n, int arr[]) {
     }
 }
 
-int main() { 
-    int n = 10, A[n];
-    
-    // Incremental array checks
-    FillInc(n, A);
-    printf("\nIncremental array:\n");
-    PrintMas(n, A);
-    printf("Run number: %d, Checksum: %d\n", RunNumber(n, A), CheckSum(n, A));
-    bubbleSort(n, A);
-    printf("\nSorted incremental array:\n");
-    PrintMas(n, A);
-    printf("Run number: %d, Checksum: %d\n", RunNumber(n, A), CheckSum(n, A));
-    
-    // Decremental array checks
-    FillDec(n, A);
-    printf("\nDecremental array:\n");
-    PrintMas(n, A);
-    printf("Run number: %d, Checksum: %d\n", RunNumber(n, A), CheckSum(n, A));
-    bubbleSort(n, A);
-    printf("\nSorted decremental array:\n");
-    PrintMas(n, A);
-    printf("Run number: %d, Checksum: %d\n", RunNumber(n, A), CheckSum(n, A));
-    
-    
-    // Random array checks
-    FillRand(n, A);
-    printf("\nRandom array:\n");
-    PrintMas(n, A);
-    printf("Run number: %d, Checksum: %d\n", RunNumber(n, A), CheckSum(n, A));
-    bubbleSort(n, A);
-    printf("\nSorted random array:\n");
-    PrintMas(n, A);
-    printf("Run number: %d, Checksum: %d\n\n", RunNumber(n, A), CheckSum(n, A));
+int main(int argc, char** argv) { 
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
 
-    printData(n, A);
+    int result = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
+    if (result < 0) {
+        SDL_Log("SDL_Init error: %s\n", SDL_GetError());
+        return -1;
+    }
     
+    window = SDL_CreateWindow("Hello world", 800, 500, 0);
+    if (window == NULL) {
+        SDL_Log("SDL_CreateWindow error: %s\n", SDL_GetError());
+        return -2;
+    }
 
+    renderer = SDL_CreateRenderer(window, NULL);
+    if (renderer == NULL) {
+        SDL_Log("SDL_CreateRenderer error: %s\n", SDL_GetError());
+        return -3;
+    }
+
+    SDL_Log("SDL3 initialized");
+
+    SDL_Event event;
+    int quit = 0;
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type)
+            {
+            case SDL_EVENT_QUIT:
+                SDL_Log("SDL3 event quit");
+                quit = 1;
+                break;
+            
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0xff, 0xff);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+        SDL_Delay(1);
+    }
+
+    SDL_Log("SDL3 shutdown");
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
